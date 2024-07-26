2eaee2439a266682814fb4bfb5708491d8384617
Charles Schaefer <charlesschaefer@gmail.com>
2024-07-26 01:08:07 -0300

    Started to add a form to the task add component

--- src/app/task/task-add/task-add.component.ts
+++ src/app/task/task-add/task-add.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
 import { Subject } from 'rxjs';
+import { TaskService } from '../../services/task.service';
+import { TaskAddDto, TaskDto } from '../../dto/task-dto';
 
 @Component({
     selector: 'app-task-add',
     standalone: true,
     imports: [
+        ReactiveFormsModule,
         OverlayPanelModule,
         CardModule,
         CalendarModule,
@@ -24,6 +28,17 @@ export class TaskAddComponent implements OnInit {
     @Output() showOverlay$Change = new EventEmitter<Subject<Event>>();
     @ViewChild('taskAddOp') taskAddOp!: OverlayPanel;
 
+    private fb = inject(FormBuilder);
+    taskForm = this.fb.group({
+        title: [null, Validators.required],
+        description: [null],
+        dueDate: [null],
+    });
+
+    constructor(
+        private taskService: TaskService<TaskAddDto>
+    ) {}
+
     ngOnInit(): void {
         this.showOverlay$.subscribe(event => {
             if (event) {
@@ -35,5 +50,9 @@ export class TaskAddComponent implements OnInit {
     onClose(event: Event) {
         this.showOverlay$.next(event);
         this.showOverlay$Change.emit(this.showOverlay$);
+    }
+
+    saveTask() {
+
     }
 }
