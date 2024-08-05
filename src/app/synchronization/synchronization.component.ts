de69842c9510a58a139c71f7d6ffba43e7073cf6
Charles Schaefer <charlesschaefer@gmail.com>
2024-08-05 08:22:25 -0300

    Debugging synchronization

--- src/app/synchronization/synchronization.component.ts
+++ src/app/synchronization/synchronization.component.ts
@@ -68,7 +68,9 @@ export class SynchronizationComponent {
         this.otpData = otp;
         console.log("OTP Data: ", otp);
         this.backupService.backupData(otp).subscribe(cryptedBackup => {
+            console.log("Data backed up. broadcasting...");
             invoke('broadcast_network_sync_services').then(() => {
+                console.log("Service broadcasted. Starting http server...");
                 invoke('start_http_server', { otp_code: otp, backup_data: cryptedBackup });
             });
         });
@@ -77,6 +79,7 @@ export class SynchronizationComponent {
     discoverDevices() {
         this.enableDiscoverButton = false;
         this.showOTPField = true;
+        console.log("Starting to discover devices...");
         // get devices with faire opened in the network
         invoke('search_network_sync_services').then(ipv4 => {
             console.log("Encountered the app in the machine with ip ", ipv4);
@@ -86,7 +89,7 @@ export class SynchronizationComponent {
     }
 
     sendEncryptedOTP(event: Event) {
-        console.log("Data: ", this.otpForm.value.otp);
+        console.log("Sending encripted data. OTP: ", this.otpForm.value.otp);
         const otp = this.otpForm.value.otp as unknown as string;
         
         // encrypts the otp and sends to the server 
@@ -97,8 +100,10 @@ export class SynchronizationComponent {
                 'X-SIGNED-TOKEN': encryptedOtp.toString()
             }
         }).subscribe(backupData => {
+            console.log("Data sent and backup received. Restoring backup...")
             this.backupService.restoreBackup(backupData, this.otpData).subscribe({
                 complete: async () => {
+                    console.log("Backup restored. Showing messages");
                     this.messageService.add({
                         summary: await firstValueFrom(this.translateService.get("Synchronized successfully")),
                         detail: await firstValueFrom(this.translateService.get("Your data was synchronized successfully.")),
@@ -108,6 +113,7 @@ export class SynchronizationComponent {
                     await this.httpClient.post(`http://${this.serverIp}:9099/disconnect`, {});
                 },
                 error: async (err) => {
+                    console.log("Error recovering backup. Showing messages...", err);
                     this.messageService.add({
                         summary: await firstValueFrom(this.translateService.get("Error synchronizing")),
                         detail: await firstValueFrom(this.translateService.get(`Error trying to synchronize data: `)) + err.toString()
