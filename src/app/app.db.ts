1918538e9502500b42bcccd007ed250e2e6ce6b3
Charles Schaefer <charlesschaefer@gmail.com>
2024-12-04 00:24:22 -0300

    Trying to solve the initial charge for the database.

--- src/app/app.db.ts
+++ src/app/app.db.ts
@@ -1,4 +1,4 @@
-import {Dexie, Table } from 'dexie';
+import {Dexie, PromiseExtended, Table } from 'dexie';
 
 import { TagDto } from './dto/tag-dto';
 import { ProjectDto } from './dto/project-dto';
@@ -6,6 +6,10 @@ import { TaskDto } from './dto/task-dto';
 import { TaskTagDto } from './dto/task-tag-dto';
 import { SettingsAddDto, SettingsDto } from './dto/settings-dto';
 import { UserDto } from './dto/user-dto';
+
+function runTaskSerial(fn: CallableFunction, results: any) {
+    return fn(results);
+}
 
 export class AppDb extends Dexie {
     task!: Table<TaskDto, number>;
@@ -39,65 +43,80 @@ export class AppDb extends Dexie {
             tag: '++id, name, uuid, user_uuid',
             task_tag: 'task, tag, user_uuid, task_uuid, tag_uuid',
             settings: '++id, notifications, todayNotifications, notificationTime, uuid, user_uuid',
-        }).upgrade(async transaction => {
+        }).upgrade(transaction => {
             try {
                 // create a new user register, since we hadn't one before
-                const users = (await transaction.table('user').toArray());
-
-                let user: UserDto;
-                if (users.length == 0) {
-                    user = await transaction.table('user').add({
+                const serialFns = {
+                    createUser: () =>transaction.table<UserDto, number>('user').add({
                         uuid: crypto.randomUUID(),
                         email: '',
                         name: '',
                         created_at: new Date()
-                    });
-                } else {
-                    user = users[0];
-                }
+                    } as UserDto, 1),
+                    getUser:(userId: number) => transaction.table<UserDto>('user').get(userId),
+                    addUuidToItems: (user: UserDto) => Promise.all([
+                        transaction.table<TaskDto, number>('task').toCollection().modify(item => {
+                            if (!item.uuid) {
+                                item.uuid = crypto.randomUUID();
+                            }
+                            item.user_uuid = user.uuid;
+                        }),
+                        transaction.table<ProjectDto, number>('project').toCollection().modify(item => {
+                            if (!item.uuid) {
+                                item.uuid = crypto.randomUUID();
+                            }
+                            item.user_uuid = user.uuid;
+                        }),
+                        transaction.table<TagDto, number>('tag').toCollection().modify(item => {
+                            if (!item.uuid) {
+                                item.uuid = crypto.randomUUID();
+                            }
+                            item.user_uuid = user.uuid;
+                        }),
+                        transaction.table<SettingsDto, number>('settings').toCollection().modify(item => {
+                            if (!item.uuid) {
+                                item.uuid = crypto.randomUUID();
+                            }
+                            item.user_uuid = user.uuid;
+                        })
+                    ]),
+                    getProjects: () => transaction.table<ProjectDto, number>('project').toArray(),
+                    getProjectMap: (projects: ProjectDto[]): Map<number, string> => {
+                        return new Map<number, string>(projects.map(project => [project.id, project.uuid]));
+                    },
+                    getTaskMap: (projectMap: Map<number, string>) => transaction.table<TaskDto>('task').toArray().then(tasks => {
+                        return [new Map<number, string>(tasks.map(task => [task.id, task.uuid])), projectMap];
+                    }),
+                    getTagMap: ([tasksMap, projectsMap]: Map<number, string>[]) => transaction.table<TagDto>('tag').toArray().then(tags => {
+                        return [tasksMap, projectsMap, new Map<number, string>(tags.map(tag => [tag.id, tag.uuid]))];
+                    }),
+                    addForeignKeysUUID: ([tasksMap, projectsMap, tagsMap]: Map<number, string>[]) => Promise.all([
+                        transaction.table('task').toCollection().modify((item: TaskDto) => {
+                            item.project_uuid = item.project ? projectsMap.get(item.project) as string : null;
+                            item.parent_uuid = item.parent ? tasksMap.get(item.parent) as string : null;
+                        }),
+                        transaction.table("task_tag").toCollection().modify((item: TaskTagDto) => {
+                            item.task_uuid = item.task ? tasksMap.get(item.task) as string : '';
+                            item.tag_uuid = item.tag ? tagsMap.get(item.tag) as string : '';
+                        })
+                    ])
+                };
                 
-                const tables = ['task', 'project', 'tag', 'settings'];
-                tables.forEach(table => {
-                    // Add uuid to all items if not already present
-                    transaction.table(table).toCollection().modify(item => {
-                        if (!item.uuid) {
-                            item.uuid = crypto.randomUUID();
-                        }
-                        item.user_uuid = user.uuid;
-                    });
-                });
-
-                const projectsMap = new Map<number, string>(
-                    (await transaction.table('project').toArray())
-                    .map(project => [project.id, project.uuid])
+                Object.values(serialFns).reduce(
+                    (promise, nextFunction) => {
+                        return promise.then(
+                            previousPromiseResult => runTaskSerial(nextFunction, previousPromiseResult)
+                        );
+                    },
+                    Promise.resolve()
                 );
-                const tasksMap = new Map<number, string>(
-                    (await transaction.table('task').toArray())
-                    .map(task => [task.id, task.uuid])
-                );
-                const tagsMap = new Map<number, string>(
-                    (await transaction.table('tag').toArray())
-                    .map(tag => [tag.id, tag.uuid])
-                );
-
-                // Fills the foreign keys with the uuid of the item
-                transaction.table('task').toCollection().modify(async (item: TaskDto) => {
-                    item.project_uuid = item.project ? projectsMap.get(item.project) as string : null;
-                    item.parent_uuid = item.parent ? tasksMap.get(item.parent) as string : null;
-                    item.user_uuid = user.uuid;
-                });
-
-                transaction.table("task_tag").toCollection().modify(async (item: TaskTagDto) => {
-                    item.task_uuid = item.task ? tasksMap.get(item.task) as string : '';
-                    item.tag_uuid = item.tag ? tagsMap.get(item.tag) as string : '';
-                    item.user_uuid = user.uuid;
-                });
             } catch (error) {
                 transaction.abort();
                 alert("Error upgrading database" + error);
                 throw error;
             }
         });
+        
 
         this.on('populate', () => this.populate());
     }
