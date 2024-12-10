2d3cdf9e35a5eab67ddd8a09a326a8fd05e55d76
Charles Schaefer <charlesschaefer@gmail.com>
2024-12-10 17:28:40 -0300

    Removing references to the ngx-indexeddb module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { NgxIndexedDBModule } from "ngx-indexed-db";
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 
 import { routes } from "./app.routes";
-import { dbConfig } from "./db.config";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -20,7 +18,6 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes),
         provideAnimationsAsync(),
         provideHttpClient(withFetch()),
-        importProvidersFrom(NgxIndexedDBModule.forRoot(dbConfig)),
         importProvidersFrom(TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
