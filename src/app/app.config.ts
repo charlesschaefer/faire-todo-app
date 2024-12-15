0b95fef14031c78f95e5037e1a1581b691bf3bf2
Charles Schaefer <charlesschaefer@gmail.com>
2024-12-15 04:20:48 -0300

    Solving an issue after mergint transloco to the main

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,19 +2,12 @@ import { ApplicationConfig, importProvidersFrom, isDevMode } from "@angular/core
 import { provideRouter } from "@angular/router";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
-import { TranslateLoader} from '@ngx-translate/core';
 import { TranslocoModule } from '@jsverse/transloco';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 
 import { routes } from "./app.routes";
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
-}
 
 export const appConfig: ApplicationConfig = {
     providers: [
