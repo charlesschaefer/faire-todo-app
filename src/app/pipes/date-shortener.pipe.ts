65f18ae15d2d8ac7ada80b3310a181a7aa90a0b7
Charles Schaefer <charlesschaefer@gmail.com>
2025-03-06 01:11:43 -0300

    Changing the date-shortener pipe to use the date locale.

--- src/app/pipes/date-shortener.pipe.ts
+++ src/app/pipes/date-shortener.pipe.ts
@@ -1,29 +1,40 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DateTime } from 'luxon';
 
+const DEFAULT_LOCALE = 'en-US';
+
 @Pipe({
-  name: 'dateShortener',
-  standalone: true,
+    name: 'dateShortener',
+    standalone: true,
 })
 export class DateShortenerPipe implements PipeTransform {
 
-  transform(value: Date, ..._args: unknown[]): string | null {
-    const now = new Date;
-    now.setHours(0);
-    now.setMinutes(0);
-    now.setSeconds(0);
-    now.setMilliseconds(0);
+    transform(value: Date, locale: string = DEFAULT_LOCALE, ..._args: unknown[]): string | null {
+        const now = new Date;
+        now.setHours(0);
+        now.setMinutes(0);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
 
-    const dtNow = DateTime.fromJSDate(now);
-    const dtValue = DateTime.fromJSDate(value);
-    
-    const diff = dtNow.diff(dtValue).as('days');
-    
-    if (diff < -1 || diff > 1) {
-      //return dtValue.toFormat("dd/MM");
-      return dtValue.toLocaleString(DateTime.DATE_SHORT);
+        if (!locale) {
+            locale = DEFAULT_LOCALE;
+        }
+
+        if (!locale.match(/\w{2}(_\w{2})?/gi)) {
+            throw new TypeError("locale can't be parsed correctly");
+        }
+
+        const dtNow = DateTime.fromJSDate(now);
+        const dtValue = DateTime.fromJSDate(value);
+
+        const diff = dtNow.diff(dtValue).as('days');
+
+        if (diff < -1 || diff > 1) {
+            dtValue.setLocale(locale);
+            //return dtValue.toFormat("dd/MM");
+            return dtValue.toLocaleString(DateTime.DATE_SHORT);
+        }
+        return dtValue.toRelativeCalendar();
     }
-    return dtValue.toRelativeCalendar();
-  }
 
 }
