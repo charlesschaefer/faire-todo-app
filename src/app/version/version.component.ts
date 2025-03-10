47ef0f69bb37627451d127d0f65990fa543096c8
Charles Schaefer <charlesschaefer@gmail.com>
2025-03-10 16:26:45 -0300

    Started to use the updater plugin to download and install a new version of the app

--- src/app/version/version.component.ts
+++ src/app/version/version.component.ts
@@ -1,14 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { check, Update } from '@tauri-apps/plugin-updater';
 
 // eslint-disable-next-line import/extensions
 import packageJson from '../../../package.json';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
     selector: 'app-version',
     standalone: true,
-    imports: [ButtonModule, RouterLink],
+    imports: [
+        ButtonModule, 
+        RouterLink,
+        ConfirmDialogModule,
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './version.component.html',
     styles: `
@@ -16,11 +24,68 @@ import packageJson from '../../../package.json';
         * {text-align: center;}
     `
 })
-export class VersionComponent {
+export class VersionComponent implements OnInit {
     version = '';
 
-    constructor() {
+    constructor(
+        private confirmationService: ConfirmationService,
+        private messageService: MessageService,
+        private translate: TranslocoService,
+    ) {
         this.version = packageJson.version
+    }
+
+    ngOnInit(): void {
+        this.checkNewVersions().then((update) => {
+            if (!update)
+                return;
+
+            this.confirmationService.confirm({
+                closable: true,
+                closeOnEscape: true,
+                header: this.translate.translate('New version available'),
+                message: this.translate.translate('Do you want to download and install the new version {{version}}?', { version: update?.version }),
+                accept: () => {
+                    this.downloadAndInstall(update);
+                }
+            })
+        });        
+    }
+
+    async checkNewVersions(): Promise<Update | void> {
+        const update = await check();
+        if (!update) {
+            console.warn("Could not check for updates");
+            return;
+        }
+        return update;
+    }
+
+    async downloadAndInstall(update: Update) {
+        console.log(`Found update ${update.version} from ${update.date} with notes ${update.body}`);
+        let downloaded = 0;
+        let contentLength = 0;
+
+        await update.downloadAndInstall((event) => {
+            switch (event.event) {
+                case 'Started': 
+                    contentLength = event.data.contentLength as number;
+                    console.log(`started downloading ${event.data.contentLength} bytes`);
+                    break;
+                
+                case 'Progress':
+                    downloaded += event.data.chunkLength;
+                    console.log(`downloaded ${downloaded} from ${contentLength}`);
+                    break;
+                
+                case 'Finished':
+                    console.log('finished downloading');
+                    break;
+            }
+        });
+
+        console.log('Update installed');
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Update installed' });
     }
 
 }
