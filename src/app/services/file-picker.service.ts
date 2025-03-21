8bd5e855ce27e65d9ad01060ac8c2fdaa4981f1f
Charles Schaefer <charlesschaefer@gmail.com>
2025-03-21 01:20:43 -0300

    Using tauri-plugin-fs to open the file, mainly because in android, plugin-dialog returns a content://  URI instead of a file path.

--- src/app/services/file-picker.service.ts
+++ src/app/services/file-picker.service.ts
@@ -4,27 +4,17 @@ import { invoke } from '@tauri-apps/api/core';
 import { AttachmentType } from '../dto/task-attachment-dto';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class FilePickerService {
-  async pickFile(): Promise<{name: string, blob: string, file_type: AttachmentType} | null> {
-    try {
-      // Open the file dialog to select a file
-      const filePath = await open({
-        multiple: false,
-        filters: [{ name: 'All Files', extensions: ['png', 'jpg', 'jpeg', 'pdf'] }],
-      });
-
-      if (filePath && typeof filePath === 'string') {
-        // Send the file path to the Rust backend to encode it to Base64
-        const base64Data: {name: string, blob: string, file_type: AttachmentType} = await invoke('encode_file_to_base64', { filePath });
-        return base64Data;
-      }
-
-      return null;
-    } catch (error) {
-      console.error('Error picking file:', error);
-      return null;
+    async pickFile(): Promise<{ name: string, blob: string, file_type: AttachmentType } | null> {
+        try {
+            // Opens a dialog to the user choose a file and then gets it's value as base64 encoded string
+            const base64Data: { name: string, blob: string, file_type: AttachmentType } = await invoke('encode_file_to_base64');
+            return base64Data;
+        } catch (error) {
+            console.error('Error picking file:', error);
+            return null;
+        }
     }
-  }
 }
