112270839ad248f85cddbd888b0ee8e1de0ccaf0
Charles Schaefer <charlesschaefer@gmail.com>
2025-04-10 03:06:32 -0300

    Changing the way we authorze supabase URLs

--- scripts/apply-supabase-url.ts
+++ scripts/apply-supabase-url.ts
@@ -2,9 +2,31 @@ const shell = require('shelljs');
 const environment = require('../src/environments/environment').environment;
 
 const DIR = require.main?.path;
-const SUPABASE_URL_PATTERN = /\$\{SUPABASE_URL\}/g;
+const SUPABASE_URL_PATTERN = /\$SUPABASE_URL/g;
+const TARGET_FILE = 'src-tauri/capabilities/opener-permissions.json';
 
-const content = shell.cat(`${DIR}/../src-tauri/capabilities/default.json`);
+const content = `
+{
+  "$schema": "../gen/schemas/desktop-schema.json",
+  "identifier": "opener-permissions",
+  "description": "Scoped permissions for the opener plugin",
+  "platforms": ["iOS", "android", "linux", "macOS", "windows"],
+  "windows": [
+    "main"
+  ],
+  "permissions": [
+    {
+      "identifier": "opener:allow-open-url",
+      "allow": [
+        {
+          "url": "$SUPABASE_URL/*"
+        }
+      ]
+    }
+  ]
+}
+`;
+
 const lines = content.split('\n');
 let found = false;
 
@@ -19,6 +41,6 @@ if (!found) {
     console.log(`Failed to find \${SUPABASE_URL} in default.json. Maybe already defined?`);
 }
 else {
-    shell.ShellString(lines.join("\n")).to(`${DIR}/../src-tauri/capabilities/default.json`);
-    console.log(`File default.json opener context updated to ${environment.supabaseUrl} ....`);
+    shell.ShellString(lines.join("\n")).to(`${DIR}/../${TARGET_FILE}`);
+    console.log(`File ${TARGET_FILE} updated to ${environment.supabaseUrl} on opener plugin context...`);
 }
